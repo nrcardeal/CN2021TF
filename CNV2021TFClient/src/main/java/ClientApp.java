68fb08c0482e3bcb1d0a8e40c8dcd5450df58f34
joanacampos3799 <joana.campos.786@gmail.com>
2021-06-17 09:46:51 +0100

    Client updated

--- CNV2021TFClient/src/main/java/ClientApp.java
+++ CNV2021TFClient/src/main/java/ClientApp.java
@@ -1,10 +1,13 @@
 import cnv2021tfservice.ImageRequest;
+import cnv2021tfservice.FilterRequest;
+import cnv2021tfservice.ImageResult;
 import cnv2021tfservice.ServiceGrpc;
 import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -38,7 +41,7 @@ public class ClientApp {
             String filename = uploadFrom.getFileName().toString();
             String contentType = Files.probeContentType(uploadFrom);
             byte[] buffer = new byte[1024];
-            try(InputStream input = Files.newInputStream(uploadFrom)) {
+            try (InputStream input = Files.newInputStream(uploadFrom)) {
                 while ((input.read(buffer)) >= 0) {
                     contents.onNext(
                             ImageRequest
@@ -50,12 +53,59 @@ public class ClientApp {
                     );
                 }
                 contents.onCompleted();
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+            if (streamObserver.isCompleted && streamObserver.success) {
+                for (ImageResult imageResult: streamObserver.results) {
+                    LabelsStreamObserver labelsObserver = new LabelsStreamObserver();
+                    noBlockStub.getLabelsList(imageResult, labelsObserver);
+                    while (!labelsObserver.isCompleted) ;
+                    if (labelsObserver.success) {
+                        System.out.println("Labels:");
+                        for (String label: labelsObserver.labels.getLabelsList()) {
+                            System.out.println(label);
+                        }
+                        System.out.println("Características:");
+                        for (String label: labelsObserver.labels.getLabelsTranslationsList()) {
+                            System.out.println(label);
+                        }
+                    }
+                }
+            }
+
+            System.out.println("Filter's initial date?");
             Scanner scan = new Scanner(System.in);
-            scan.nextLine();
-        } catch (Exception e) {
+            String initDate = scan.nextLine();
+            System.out.println("Filter's end date?");
+            scan = new Scanner(System.in);
+            String endDate = scan.nextLine();
+            System.out.println("Filter's label");
+            scan = new Scanner(System.in);
+            String label = scan.nextLine();
+
+            FilterRequest request = FilterRequest
+                    .newBuilder()
+                    .setInitDate(initDate)
+                    .setEndDate(endDate)
+                    .setLabel(label)
+                    .build();
+                FilterStreamObserver filterObserver = new FilterStreamObserver();
+                noBlockStub.filterFiles(request, filterObserver);
+                while (!filterObserver.isCompleted);
+                if (filterObserver.success) {
+                    System.out.println("Filtered Files:");
+                    for ( String name: filterObserver.result.getFilenameList()) {
+                        System.out.println(name);
+                    }
+                    scan = new Scanner(System.in);
+                    scan.nextLine();
+                }
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
-    }
 
+    }
 }
